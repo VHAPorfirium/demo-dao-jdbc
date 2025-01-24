16ccfaff1db6111859fab732052db2452239cc4c
Victor Hugo <vhaporfiro@gmail.com>
2025-01-24 19:55:29 -0300

    insert implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -3,6 +3,7 @@ package model.dao.impl;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,6 +13,7 @@ import db.DB;
 import db.DbException;
 
 import java.sql.Connection;
+
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -26,8 +28,46 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void insert(Seller obj) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+        PreparedStatement st = null;
+
+        try {
+            st = conn.prepareStatement(
+                "INSERT INTO seller "
+                + "(Name, Email, BirthDate, BaseSalary, DepartmentId) " 
+                + "VALUES " 
+                + "(?, ?, ?, ?, ?)",
+                Statement.RETURN_GENERATED_KEYS);
+
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                
+                if(rs.next()) {
+                    int id = rs.getInt(1);
+                    obj.setId(id);
+                }
+                DB.closeResultSet(rs);
+            }
+            else {
+                throw new DbException("Unexpected! No rows affected");
+            }
+
+        } catch (SQLException e) {
+
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatements(st);
+        }
+
+        
     }
 
     @Override
