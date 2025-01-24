d97e3ad879de3c637005a163b968768f7665c248
Victor Hugo <vhaporfiro@gmail.com>
2025-01-24 10:08:23 -0300

    Classe seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import entities.Department;
+import entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
 
-        System.out.println(obj);
+        Seller seller1 = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller1);
     }
     
 }
